1ee4dfe1ce9c9626b8247f87b2cac110a1f48d87
Edmund Yu <38992135+Nolelle@users.noreply.github.com>
2024-10-22 22:27:22 -0600

    first commit

---
+++ src/shapes/Shape.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package shapes;
+
+/**
+ * 
+ */
+public class Shape implements Comparable<Shape> {
+	private double height;
+	
+	public Shape (double height) {
+		this.height = height;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+	
+	public void setHeight(double height) {
+		this.height = height;
+	}
+	
+	public abstract double calcBaseArea;
+	public abstract double calcVolume;
+	
+	
+	@Override
+	public int compareTo(Shape other) {
+		if (this.height > other.height) return 1;
+		 else if (this.height < other.height) return -1;
+		return 0;
+	} 
+	
+
+}
